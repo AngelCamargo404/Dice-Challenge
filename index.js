f86ef987e64a9dac6ef698debdbf8b823b3ff79f
Angel Camargo <adcamargo10@gmail.com>
2022-12-22 14:26:06 -0400

    Changing the way users play

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
+var dices = document.querySelectorAll('.dice');
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+dices.forEach(dice => {
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+    dice.addEventListener('click', function() {
+      var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+      var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-var image1 = document.querySelectorAll("img")[0];
+      var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
 
-image1.setAttribute("src", randomImageSource);
+      var image1 = document.querySelectorAll("img")[0];
 
-
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+      image1.setAttribute("src", randomImageSource);
 
 
-//If player 1 wins
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
-}
-else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-}
-else {
-  document.querySelector("h1").innerHTML = "Draw!";
-}
+      var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+      var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+      document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+      //If player 1 wins
+      if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+      }
+      //If player 2 wins
+      else if (randomNumber2 > randomNumber1) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+      }
+      //If it's a draw
+      else {
+        document.querySelector("h1").innerHTML = "Draw!";
+      }
+
+    });
+
+});
+
